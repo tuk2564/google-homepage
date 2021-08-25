5a3ac0025a90f502c8f1a70b217a2d1d3af3d25f
tuk2564 <tuk2564@outlook.com>
2021-08-25 17:07:05 +0700

    add config files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <div className='App'></div>
+      <div></div>
+      <button>test</button>
+    </>
   );
 }
 
